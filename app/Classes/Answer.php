5bc248d872788a967d603189b4f67f8ad41b720c
a.bunyza <a.bunyza@gmail.com>
2017-04-08 20:50:49 +0300

    fix error

--- app/Classes/Answer.php
+++ app/Classes/Answer.php
@@ -265,11 +265,10 @@ class Answer
         } elseif (preg_match('#\(%o2\)\s+(.*)\s*\(%i3\)#iUs', $value, $found)) {
             $found = preg_replace('#\s+#', '', $found[1]);
             // var_dump($found);
-            return preg_replace('#.0$#', '', $found);
+            return preg_replace('#\.0$#', '', $found);
         } elseif (preg_match('#\(%o1\)(.*)\(%i2\)#iUs', $value, $found)) {
             $found = preg_replace('#\s+#', '', $found[1]);
-            // var_dump($found);
-            return preg_replace('#.0$#', '', $found);
+            return preg_replace('#\.0$#', '', $found);
         }
         return false;
 
